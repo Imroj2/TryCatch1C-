9e25a29145d6e9124bddcc5763ba06edecbf8d0d
Dell <Dell@DESKTOP-FM8464J>
2024-01-10 07:40:34 +0545

    Add project files.

---
+++ TryCatch1C#/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int[] array = new int[6];
+            int value = array[10];
+            try
+            {
+                //nested try block
+                int x = 10;
+                Console.WriteLine(x / 0);        
+            }
+            catch (Exception e)
+            {
+                   Console.WriteLine("Exception caught: {0}", e);
+            
+            }
+        }
+        catch(IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("IndexOutOfRangeException:" + ex.Message);
+        }
+        finally
+        {
+            //cleanup code {always executed
+            Console.WriteLine("Finnaly block is executed");
+        }
+    }
+}
